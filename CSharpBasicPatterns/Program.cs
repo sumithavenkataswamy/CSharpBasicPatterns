1b4860bbc3ff7cdd0827e42015aaa4e91b6447da
Sumitha V <HappyGowda23@gmail.com>
2025-03-15 20:21:46 +0530

    Added signleton pattern

---
+++ CSharpBasicPatterns/Program.cs
@@ -0,0 +1,41 @@
+﻿using CSharpBasicPatterns.SingletonPattern;
+
+var instance = ProductConfiguration.GetProductConfigurationInstance();
+
+instance.SetProductConfiguration(new Configuration()
+{
+    ProductName = "Laptop",
+    ProductVersion = "1.0.0",
+});
+
+PrintProdConfiguration();
+
+void PrintProdConfiguration()
+{
+    foreach (var item in instance.getProductConfigurations())
+    {
+        Console.WriteLine($"ProductName: {item.ProductName} ProductVersion: {item.ProductVersion}");
+    }
+}
+
+#region Multithreads
+
+//static void CreateInstance() => ProductConfiguration.GetProductConfigurationInstance();
+//// Create an array of threads
+//Thread[] threads = new Thread[5];
+
+//// Initialize and start multiple threads
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(CreateInstance);
+//    threads[i].Start();
+//}
+
+//// Wait for all threads to complete
+//foreach (Thread thread in threads)
+//{
+//    thread.Join();
+//}
+
+//Console.WriteLine("All threads have completed execution.");
+#endregion
---
+++ CSharpBasicPatterns/SingletonPattern/ProductConfiguration.cs
@@ -0,0 +1,49 @@
+﻿public sealed class Configuration
+{
+    public string ProductName { get; set; }
+    public string ProductVersion { get; set; }
+}
+
+namespace CSharpBasicPatterns.SingletonPattern
+{
+    public sealed class ProductConfiguration
+    {
+
+        private List<Configuration> _configurations = new();
+        private static ProductConfiguration _instance = null;
+        private static readonly object _instanceLock = new object();
+
+        private ProductConfiguration() { }        
+        
+        public static ProductConfiguration GetProductConfigurationInstance()
+        {
+            if (_instance == null)
+            {
+                Console.WriteLine("instance is null here");
+
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        Console.WriteLine("ProductConfiguration instace created");
+                        _instance = new ProductConfiguration();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public List<Configuration> getProductConfigurations()
+        {
+            Console.WriteLine("Product configuration get method called");
+            return _configurations;
+        }
+
+        public void SetProductConfiguration(Configuration configuration)
+        {
+            _configurations.Add(configuration);
+            Console.WriteLine("Production configuration set method called");
+        }
+    }
+}
